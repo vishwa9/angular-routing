6ad2ca6f2c5084fb36c3fa75135f906324b2dd93
vishwajeet singh chauhan <vishwajeet11104736@gmail.com>
2019-03-19 02:40:13 +0530

    initial commit

---
+++ src/app/users/user/user.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit, OnDestroy {
+  user: {id: number, name: string};
+  paramSubscription: Subscription;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    // console.log(this.route);
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    }
+
+    this.paramSubscription = this.route.params.subscribe(
+      (params: Params) => {
+        this.user.id = params.id;
+        this.user.name = params.name;
+      }
+    );
+  }
+  
+  ngOnDestroy(){
+    this.paramSubscription.unsubscribe();
+  }
+}
